79480eed5c8849cede779c58e4c8a3f81c2d32dd
Kavudhami <abiandabi92@gmail.com>
2016-03-17 14:39:53 -0500

    checks if there are duplicates in an array
    if a number is repeated twice, return true; else false; TimeComplexity: O(n)

---
+++ ContainsDuplicate.java
@@ -0,0 +1,19 @@
+/*Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears
+at least twice in the array, and it should return false if every element is distinct.*/
+
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
+       if(nums.length<=1)
+       return false;
+       for(int i=0;i<nums.length;i++)
+       if(!h.containsKey(nums[i]))
+       
+           h.put(nums[i],1);
+           
+       else
+       return true;
+       return false;
+    }
+}
